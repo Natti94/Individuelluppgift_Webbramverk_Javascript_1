00289113179cb5f4d33a8ee49fd7b288ff77fb39
Natti94 <natnael.berhane.sv@gmail.com>
2025-02-28 15:31:25 +0100

    update

--- weather-app/src/components/map.jsx
+++ weather-app/src/components/map.jsx
@@ -1,9 +1,13 @@
-import App from './App.js'
+import App from './App.jsx'
+import { MapContainer } from "react-leaflet";
 
 const Map = () => {
-
-
-    
-}
+  return (
+    // London
+<App>
+    <MapContainer center={[51.5074, -0.1278]} zoom={10} />
+</App>
+      );
+};
 
 export default Map;
